64d741d1fe405bedb8ee9dd3929c3040c0706b98
KanishkaNaik <kanishkabc123@gmail.com>
2022-08-21 19:53:29 +0530

    RXJS BufferToggle Operator.Learn BufferToggle Operator in Trasnformation Category.

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval,  } from 'rxjs';
-import { bufferToggle, take, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-operators',
@@ -10,18 +9,12 @@ import { bufferToggle, take, tap } from 'rxjs/operators';
 export class OperatorsComponent implements OnInit {
 
 
-   Operators:any[]=["buffer","bufferCount","bufferTimerCount","bufferToggle"]
+   Operators:any[]=["buffer","bufferCount","bufferTimerCount","bufferToggle","bufferWhen"]
   constructor() { }
 
   ngOnInit(): void {
 
-    let opening = interval(6000).pipe(tap(()=>{console.log('Open');}))
-    let closing  = ()=>interval(3000).pipe(tap(()=>{console.log('Close');}))
 
-    interval(1000).pipe(tap((data)=>{console.log(data);}),bufferToggle(opening,closing), take(3)).subscribe((data)=>{
-      console.log(data);
-      
-    })
   }
 
 }
--- src/app/operators/pages/buffer-toggle-operator/buffer-toggle-operator.component.ts
+++ src/app/operators/pages/buffer-toggle-operator/buffer-toggle-operator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval,  } from 'rxjs';
+import { bufferToggle, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-buffer-toggle-operator',
@@ -10,6 +12,13 @@ export class BufferToggleOperatorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let opening = interval(6000).pipe(tap(()=>{console.log('Open');}))
+    let closing  = ()=>interval(3000).pipe(tap(()=>{console.log('Close');}))
+
+    interval(1000).pipe(tap((data)=>{console.log(data);}),bufferToggle(opening,closing), take(3)).subscribe((data)=>{
+      console.log(data);
+      
+    })
   }
 
 }
