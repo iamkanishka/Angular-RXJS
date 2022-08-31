42371f6d7107f53ff23e5c7d9b603709809d2bd1
KanishkaNaik <kanishkabc123@gmail.com>
2022-08-31 19:44:33 +0530

    79. RXJS Range Operator.Learn RXJS Creattion Operator Range Operator - RXJS.

---
+++ src/app/operators/pages/range-operator/range-operator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-range-operator',
+  templateUrl: './range-operator.component.html',
+  styleUrls: ['./range-operator.component.scss']
+})
+export class RangeOperatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
